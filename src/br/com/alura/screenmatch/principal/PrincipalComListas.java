25827fff6251c9cc4a75dfd2d511f4830842e24a
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-01-28 15:41:59 -0300

    Update PrincipalComListas.java

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -9,8 +9,11 @@ import java.util.ArrayList;
 public class PrincipalComListas {
     public static void main(String[] args) {
         Filme meuFilme = new Filme("O poderoso chefão", 1970);
+        meuFilme.avalia(8);
         Filme outroFilme = new Filme("Doomsday", 2026);
+        outroFilme.avalia(9);
         var filmedoGustavo = new Filme("No Way Home", 2021);
+        filmedoGustavo.avalia(10);
         Serie twd = new Serie("The Walking Dead", 2010);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -20,7 +23,10 @@ public class PrincipalComListas {
         lista.add(twd);
         for (Titulo item: lista){
             System.out.println(item);
+            Filme filme = (Filme) item;
+            System.out.println("Classificação" + filme.getClassificao();
             
         }
     }
 }
+
