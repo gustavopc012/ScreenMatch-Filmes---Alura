cf8f810962fffe34d5a21222ffc7a6290e752abe
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-02-05 16:54:45 -0300

    Update Titulo.java

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -78,11 +78,14 @@ public class Titulo implements Comparable<Titulo> {
         return this.getNome().compareTo(outroTitulo.getNome());
     }
 
-
     @Override
     public String toString() {
         return "nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento;
+                ", anoDeLancamento=" + anoDeLancamento + "," +
+                "duração " + duracaoEmMinutos;
+
 
     }
 }
+
+
