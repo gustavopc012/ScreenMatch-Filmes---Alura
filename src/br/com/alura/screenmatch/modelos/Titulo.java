9f6dc8045e2e0e3bee80a83ba4376273441a19a3
Gustavo Cordeiro <107758518+gustavopc012@users.noreply.github.com>
2026-02-10 14:54:22 -0300

    Refactor Titulo class with improved formatting

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -85,10 +85,11 @@ public class Titulo implements Comparable<Titulo> {
 
        @Override
     public String toString() {
-        return "(nome='" + nome + '\'' +
-                ", anoDeLancamento=" + anoDeLancamento + "," +
-                "duração " + duracaoEmMinutos + ")";
+        return "(nome = " + nome  +
+                ", anoDeLancamento = " + anoDeLancamento + "," +
+                "duração = " + duracaoEmMinutos + ")";
 
     }
 }
+
 
